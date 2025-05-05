0b43df13163e79aa58c21a0e9e1e8ce58c233a46
firnanda <ahmadnanda402@gmail.com>
2025-05-05 17:35:17 +0700

    crud pada domain logicnnya udah

--- Program.cs
+++ Program.cs
@@ -1,10 +1,55 @@
-﻿namespace AKMJ_TubesKPL
+﻿using System;
+
+
+namespace AKMJ_TubesKPL
 {
     public class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
+            //    var repository = new TodoRepository();
+            //    var service = new TodoService(repository);
 
+            //    bool exit = false;
+            //    while (!exit)
+            //    {
+            //        Console.Clear();
+            //        Console.WriteLine("=== Selamat datang pada To-Do List ===");
+            //        Console.WriteLine("1. Lihat Daftar To-Do");
+            //        Console.WriteLine("2. Tambah To-Do Baru");
+            //        Console.WriteLine("3. Edit To-Do");
+            //        Console.WriteLine("4. Hapus To-Do");
+            //        Console.WriteLine("5. Tandai Selesai");
+            //        Console.WriteLine("6. Keluar");
+            //        Console.Write("Pilih menu: ");
+
+            //        var choice = Console.ReadLine();
+            //        switch (choice)
+            //        {
+            //            case "1":
+            //                service.ViewAll();
+            //                break;
+            //            case "2":
+            //                service.AddNew();
+            //                break;
+            //            case "3":
+            //                service.Edit();
+            //                break;
+            //            case "4":
+            //                service.Delete();
+            //                break;
+            //            case "5":
+            //                service.ToggleComplete();
+            //                break;
+            //            case "6":
+            //                exit = true;
+            //                break;
+            //            default:
+            //                Console.WriteLine("Pilihan tidak valid!");
+            //                Console.ReadKey();
+            //                break;
+            //        }
+            //    }
         }
     }
 }
